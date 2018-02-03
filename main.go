fdcd4020dda4d1e20682c7d7f23d33ef4ca3ab99
davidmdm <david.desmarais-michaud@pj.ca>
2018-02-02 23:31:15 -0500

    Approaching the command line interface in the assingment

--- main.go
+++ main.go
@@ -10,35 +10,64 @@ import (
 	"strings"
 )
 
+type header map[string]string
+
+var colon, _ = regexp.Compile(":")
+
+func (h header) String() string {
+	s := "{\n"
+	for key, value := range h {
+		s += fmt.Sprintf("  %s: %v\n", key, value)
+	}
+	s += "}"
+	return s
+}
+
+func (h header) Set(s string) error {
+	if !colon.MatchString(s) {
+		return fmt.Errorf("Header value must contain a key:value pair; recieved: %s", s)
+	}
+	header := strings.Split(s, ":")
+	h[header[0]] = header[1]
+	return nil
+}
+
 func main() {
 
-	method := flag.String("v", "GET", "The http verb")
+	var headerMap header
+
+	flag.Var(headerMap, "h", "header key:value pair")
+
 	port := flag.String("p", "80", "The connection port")
-	uri := flag.String("u", "", "The uri of the request")
+	verbose := flag.Bool("v", false, "Verbose mode")
 
 	flag.Parse()
 
-	*method = strings.ToUpper(*method)
-
-	if *method != "GET" && *method != "POST" {
-		log.Fatalf("Method not supported: %v\n", *method)
+	method := strings.ToUpper(flag.Arg(0))
+	if method != "GET" && method != "POST" {
+		log.Fatalf("Expecting method to be GET or POST, got: %s", method)
 	}
 
-	if *uri == "" {
-		log.Fatalln("uri is required")
+	uri := flag.Arg(1)
+	if uri == "" {
+		log.Fatal("Uri is required")
+	}
+
+	if method != "GET" && method != "POST" {
+		log.Fatalf("Method not supported: %v\n", method)
 	}
 
 	r, _ := regexp.Compile("/")
 
-	pathIndex := r.FindStringIndex(*uri)
+	pathIndex := r.FindStringIndex(uri)
 
 	var host string
 	var path string
 	if len(pathIndex) == 0 {
-		host = *uri
+		host = uri
 		path = "/"
 	} else {
-		temp := *uri
+		temp := uri
 		host = temp[:pathIndex[0]]
 		path = temp[pathIndex[0]:]
 	}
@@ -48,7 +77,13 @@ func main() {
 		log.Fatalf("connection could no be established: %v", err)
 	}
 
-	fmt.Fprintf(conn, fmt.Sprintf("%s %s HTTP/1.0\r\nHost: %s\r\n\r\n", *method, path, host))
+	protocol := fmt.Sprintf("%s %s HTTP/1.0\r\nHost: %s\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: 13\r\n\r\nsay=Hi&to=Mom", method, path, host)
+
+	if *verbose == true {
+		fmt.Println(protocol, "\n\n")
+	}
+
+	fmt.Fprintf(conn, protocol)
 
 	bs, err := ioutil.ReadAll(conn)
 	if err != nil {
